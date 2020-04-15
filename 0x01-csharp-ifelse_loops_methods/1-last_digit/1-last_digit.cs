73e932bb4d51865a34583cd623900354e18cb9d1
Marco Chan <inspiredtolive0@gmail.com>
2020-04-14 21:09:43 -0700

    Prints the last digit of a rand num

---
+++ 0x01-csharp-ifelse_loops_methods/1-last_digit/1-last_digit.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Random rndm = new Random();
+        int number = rndm.Next(-10000, 10000);
+        int lastDigit = number % 10;
+        string msg = "";
+        if (lastDigit > 5) {
+            msg = "and is greater than 5";
+        } else if (lastDigit == 0) {
+            msg = "and is 0";
+        } else {
+            msg = "and is less than 6 and not 0";
+        }
+	    Console.WriteLine("The last digit of {0} is {1} {2}", number, lastDigit, msg);
+    }
+}
