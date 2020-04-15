e0af61f197d8b684523730a885fa07b6e8a6890e
Marco Chan <inspiredtolive0@gmail.com>
2020-04-14 21:54:32 -0700

    Prints unique combinations between 00-99

---
+++ 0x01-csharp-ifelse_loops_methods/6-print_comb2/6-print_comb2.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace _6_print_comb2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string sep = "";
+            int first = 0, second = 0; 
+            for (int i = 0; i < 100; i++) {
+                first = i / 10 % 10;
+                second = i % 10;
+                if (first < second) {
+                    Console.Write("{0}{1:D2}", sep, i);
+                    sep = ", ";
+                }
+            }
+            Console.Write("\n");
+        }
+    }
+}
