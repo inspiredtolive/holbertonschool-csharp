593935d34ed08495edbfdfca18f7a5e8e8398494
Marco Chan <inspiredtolive0@gmail.com>
2020-04-14 20:46:19 -0700

    Prints a random num's sign

---
+++ 0x01-csharp-ifelse_loops_methods/0-positive_or_negative/0-positive_or_negative.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Random rndm = new Random();
+        int number = rndm.Next(-10, 10);
+        string sign = "";
+	    if (number > 0) {
+            sign = "positive";
+        } else if (number == 0) {
+            sign = "zero";
+        } else {
+            sign = "negative";
+        }
+        Console.WriteLine("{0} is {1}", number, sign);
+    }
+}
