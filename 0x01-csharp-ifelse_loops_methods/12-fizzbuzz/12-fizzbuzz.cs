f2f1766500e15d9c15a9a04e18ba0911bdf2159e
Marco Chan <inspiredtolive0@gmail.com>
2020-04-14 23:04:55 -0700

    Prints FizzBuzz 1-100

---
+++ 0x01-csharp-ifelse_loops_methods/12-fizzbuzz/12-fizzbuzz.cs
@@ -0,0 +1,35 @@
+using System;
+
+namespace _12_fizzbuzz
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            for (int i = 1; i < 101; i++)
+            {
+                if (i % 15 == 0)
+                {
+                    Console.Write("FizzBuzz");
+                } 
+                else if (i % 3 == 0)
+                {
+                    Console.Write("Fizz");
+                }
+                else if (i % 5 == 0)
+                {
+                    Console.Write("Buzz");
+                }
+                else
+                {
+                    Console.Write(i);
+                }
+                if (i != 100)
+                {
+                    Console.Write(" ");
+                }
+            }
+            Console.WriteLine();
+        }
+    }
+}
