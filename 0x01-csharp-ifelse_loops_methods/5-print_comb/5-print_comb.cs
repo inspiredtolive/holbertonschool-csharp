3f0cc98ecaa16adf9e70ad1f1aa64b1edfa853de
Marco Chan <inspiredtolive0@gmail.com>
2020-04-14 21:48:25 -0700

    Prints 00-99

---
+++ 0x01-csharp-ifelse_loops_methods/5-print_comb/5-print_comb.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace _5_print_comb
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string sep = "";
+            for (int i = 0; i < 100; i++) {
+                Console.Write("{0}{1:D2}", sep, i);
+                sep = ", ";
+            }
+            Console.Write("\n");
+        }
+    }
+}
