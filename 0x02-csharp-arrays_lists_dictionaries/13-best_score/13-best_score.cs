cb3a41ede2b93c292792faf1fd8cd7c601008a66
Marco Chan <inspiredtolive0@gmail.com>
2020-04-16 23:38:00 -0700

    Returns the key with the highest value

---
+++ 0x02-csharp-arrays_lists_dictionaries/13-best_score/13-best_score.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+
+class Dictionary
+{
+    public static string BestScore(Dictionary<string, int> myList)
+    {
+        string maxKey = null;
+
+        if (myList.Count == 0)
+        {
+            return null;
+        }
+        foreach (string key in myList.Keys)
+        {
+            if (maxKey == null || myList[maxKey] < myList[key])
+            {
+                maxKey = key;
+            }
+        }
+        return maxKey;
+    }
+}
