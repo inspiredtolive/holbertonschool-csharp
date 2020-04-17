9b9259d6bc4fda363d138829565f0effd304452e
Marco Chan <inspiredtolive0@gmail.com>
2020-04-16 21:19:45 -0700

    Prints an array in reverse

---
+++ 0x02-csharp-arrays_lists_dictionaries/3-print_array_reverse/3-print_array_reverse.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+class Array
+{
+    public static void Reverse(int[] array)
+    {
+        String sep = "";
+        if (array != null)
+        {
+            for (int i = array.Length - 1; i >= 0; i--)
+            {
+                Console.Write("{0}{1}", sep, array[i]);
+                sep = " ";
+            }
+        }
+        Console.WriteLine();
+    }
+}
