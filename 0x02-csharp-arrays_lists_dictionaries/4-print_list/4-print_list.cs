da6e2d8d3869419474d4a879ca2b3e1679b1778d
Marco Chan <inspiredtolive0@gmail.com>
2020-04-16 21:51:18 -0700

    Creates and prints a list

---
+++ 0x02-csharp-arrays_lists_dictionaries/4-print_list/4-print_list.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+
+class List
+{
+    public static List<int> CreatePrint(int size)
+    {
+        List<int> list;
+        string sep = "";
+
+        if (size < 0)
+        {
+            Console.WriteLine("Size cannot be negative");
+            return null;
+        }
+        list = new List<int>();
+        for (int i = 0; i < size; i++)
+        {
+            list.Add(i);
+            Console.Write("{0}{1}", sep, i);
+            sep = " ";
+        }
+        Console.WriteLine();
+        return list;
+    }
+}
