2727861c4921cf6f6b983d63e5acbc7a0f84cc67
Marco Chan <inspiredtolive0@gmail.com>
2020-04-16 23:17:03 -0700

    Creates a new dictionary with values doubled

---
+++ 0x02-csharp-arrays_lists_dictionaries/11-multiply_by_2/11-multiply_by_2.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+
+class Dictionary
+{
+    public static Dictionary<string, int> MultiplyBy2(Dictionary<string, int> myDict)
+    {
+        Dictionary<string, int> doubleDict = new Dictionary<string, int>();
+
+        foreach (string key in myDict.Keys)
+        {
+            doubleDict.Add(key, myDict[key] * 2);
+        }
+        return doubleDict;
+    }
+}
