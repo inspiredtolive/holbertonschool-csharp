50e0edada09c9c1a8d3fb417a3e3ca79d4f0e5ff
Marco Chan <inspiredtolive0@gmail.com>
2020-04-17 00:04:34 -0700

    Prints a jagged array

---
+++ 0x02-csharp-arrays_lists_dictionaries/100-jagged_array/100-jagged_array.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        int[][] array = {
+            new int[] { 0, 1, 2, 3 },
+            new int[] { 0, 1, 2, 3, 4, 5, 6 },
+            new int[] { 0, 1 }
+        };
+        string sep = "";
+
+        for (int i = 0; i < 3; i++)
+        {
+            sep = "";
+            for (int j = 0; j < array[i].Length; j++)
+			{
+				Console.Write("{0}{1}", sep, array[i][j]);
+                sep = " ";
+			}
+            Console.WriteLine();
+        }
+    }
+}
