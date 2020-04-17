192c6d84edf96eca9f1dfce90bccdb967cf85c02
Marco Chan <inspiredtolive0@gmail.com>
2020-04-16 23:51:32 -0700

    Prints a 2d array

---
+++ 0x02-csharp-arrays_lists_dictionaries/14-rectangular_array/14-rectangular_array.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        int[,] array = new int[5, 5];
+
+        array[2, 2] = 1;
+        for (int i = 0; i < 5; i++)
+        {
+            string sep = "";
+            for (int j = 0; j < 5; j++)
+            {
+                Console.Write("{0}{1}", sep, array[i, j]);
+                sep = " ";
+            }
+            Console.WriteLine();
+        }
+    }
+}
