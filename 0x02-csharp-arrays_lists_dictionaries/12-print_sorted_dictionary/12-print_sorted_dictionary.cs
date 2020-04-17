071489a59537adda1fa3fcb07d1415e7e68708d7
Marco Chan <inspiredtolive0@gmail.com>
2020-04-16 23:30:33 -0700

    Prints a dictionary ordered by keys

---
+++ 0x02-csharp-arrays_lists_dictionaries/12-print_sorted_dictionary/12-print_sorted_dictionary.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+
+class Dictionary
+{
+    public static void PrintSorted(Dictionary<string, string> myDict)
+    {
+        SortedList<string, string> keys = new SortedList<string, string>(myDict);
+
+        foreach (KeyValuePair<string, string> kvp in keys)
+        {
+            Console.WriteLine("{0}: {1}", kvp.Key, kvp.Value);
+        }
+    }
+}
