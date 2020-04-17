df9c81618319da297b15d8466936edcaf90fd405
Marco Chan <inspiredtolive0@gmail.com>
2020-04-16 19:42:30 -0700

    Creates and prints an array of ints

---
+++ 0x02-csharp-arrays_lists_dictionaries/0-print_array/0-print_array.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+class Array
+{
+    public static int[] CreatePrint(int size)
+    {
+        int[] array;
+        if (size < 0)
+        {
+            Console.WriteLine("Size cannot be negative");
+            return null;
+        }
+        array = new int[size];
+        for (int i = 0; i < size; i++)
+        {
+            if (i > 0)
+            {
+                Console.Write(" ");
+            }
+            Console.Write(i);
+            array[i] = i;         
+        }
+        Console.WriteLine();
+        return array;
+    }
+}
