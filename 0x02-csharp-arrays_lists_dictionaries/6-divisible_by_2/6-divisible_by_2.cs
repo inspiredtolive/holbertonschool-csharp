90efa0a80b44f38458d9191f71706290a3447bbe
Marco Chan <inspiredtolive0@gmail.com>
2020-04-16 22:28:53 -0700

    Checks if all ints in a list is even

---
+++ 0x02-csharp-arrays_lists_dictionaries/6-divisible_by_2/6-divisible_by_2.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+
+class List
+{
+    public static List<bool> DivisibleBy2(List<int> myList)
+    {
+        List<bool> isEvenList = new List<bool>();
+
+        foreach (int num in myList)
+        {
+            if (num % 2 == 0)
+            {
+                isEvenList.Add(true);
+            }
+            else
+            {
+                isEvenList.Add(false);
+            }
+        }
+        return isEvenList;
+    }
+}
