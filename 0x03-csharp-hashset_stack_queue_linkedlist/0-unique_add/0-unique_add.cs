084037ddc911c96b2efc81281c7126ed557bcf5f
Marco Chan <inspiredtolive0@gmail.com>
2020-04-27 15:43:14 -0700

    Computes sum of unique ints in a list

---
+++ 0x03-csharp-hashset_stack_queue_linkedlist/0-unique_add/0-unique_add.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+
+class List
+{
+    // Main - entry point
+    public static int Sum(List<int> myList)
+    {
+        int sum = 0;
+        HashSet<int> uniqueNums = new HashSet<int>(myList);
+
+        foreach (int num in uniqueNums)
+        {
+            sum += num;
+        }
+        return sum;
+    }
+}
