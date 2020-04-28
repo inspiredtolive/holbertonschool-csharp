7d6d749866b2976756420d42bfda04ba70c8308b
Marco Chan <inspiredtolive0@gmail.com>
2020-04-27 18:35:07 -0700

    Removes the first node in a linked list

---
+++ 0x03-csharp-hashset_stack_queue_linkedlist/9-linkedlist_pop/9-linkedlist_pop.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+
+class LList
+{
+    public static int Pop(LinkedList<int> myLList)
+    {
+        int val;
+
+        if (myLList.Count == 0)
+        {
+            return (0);
+        }
+        val = myLList.First.Value;
+        myLList.RemoveFirst();
+        return val;
+    }
+}
