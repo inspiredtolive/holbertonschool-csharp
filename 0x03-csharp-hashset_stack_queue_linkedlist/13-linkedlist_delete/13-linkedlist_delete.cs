6432dbae93f283a89250f747571b8c11a4155230
Marco Chan <inspiredtolive0@gmail.com>
2020-04-27 19:07:13 -0700

    Deletes a node from a linked list

---
+++ 0x03-csharp-hashset_stack_queue_linkedlist/13-linkedlist_delete/13-linkedlist_delete.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+
+class LList
+{
+    public static void Delete(LinkedList<int> myLList, int index)
+    {
+        LinkedListNode<int> current = myLList.First;
+
+		if (index >= myLList.Count || index < 0)
+		{
+			return;
+		}
+		for (int i = 0; i < index; i++, current = current.Next)
+            ;
+		myLList.Remove(current);
+    }
+}
