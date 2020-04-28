f87dd969a25a51a05eb0bfaebc585482ea7c124f
Marco Chan <inspiredtolive0@gmail.com>
2020-04-27 18:54:30 -0700

    Gets a nodes from a linked list

---
+++ 0x03-csharp-hashset_stack_queue_linkedlist/10-linkedlist_get_node/10-linkedlist_get_node.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+class LList
+{
+    public static int GetNode(LinkedList<int> myLList, int n)
+    {
+        LinkedListNode<int> node;
+
+        if (n < 0 || n > myLList.Count - 1)
+        {
+            return 0;
+        }
+        node = myLList.First;
+        for (; n > 0; node = node.Next, n--)
+            ;
+        return node.Value;
+    }
+}
