29074fd76252c2e92cfbf308b1230409d3704705
Marco Chan <inspiredtolive0@gmail.com>
2020-04-30 20:17:45 -0700

    Creates a getter for health

---
+++ 0x06-csharp-classes/3-enemy/3-enemy.cs
@@ -0,0 +1,42 @@
+﻿using System;
+
+namespace Enemies
+{
+    /// <summary>
+    /// Represents a Zombie.
+    /// </summary>
+    class Zombie 
+    {
+        int health;
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="Zombie"/> class.
+        /// </summary>
+        public Zombie()
+        {
+            health = 0;
+        }
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="Zombie"/> class.
+        /// </summary>
+        /// <param name="value">The Zombie's health value.</param>
+        public Zombie(int value)
+        {
+            if (value < 0)
+            {
+                throw new System.ArgumentException("Health must be greater than or equal to 0");
+            }
+            health = value;
+        }
+
+        /// <summary>
+        /// Gets the health of the zombie.
+        /// </summary>
+        /// <returns>The health of the zombie.</returns>
+        public int GetHealth()
+        {
+            return health;
+        }
+    }
+}
