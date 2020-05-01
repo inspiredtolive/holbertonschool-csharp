7a40ccd8b82fa73e4533c679a7e04f70d71e7a70
Marco Chan <inspiredtolive0@gmail.com>
2020-04-30 19:34:25 -0700

    Adds health field to Zombie

---
+++ 0x06-csharp-classes/1-enemy/1-enemy.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace Enemies
+{
+    /// <summary>Represents a Zombie.</summary>
+    class Zombie 
+    {
+        public int health;
+
+        /// <summary>Initializes a new instance of the <see cref="Zombie"/> class.</summary>
+        public Zombie()
+        {
+            health = 0;
+        }
+    }
+}
