8b693f2bca7d808e7da2fffe9961f232d44d8fb0
Marco Chan <inspiredtolive0@gmail.com>
2020-04-30 21:28:11 -0700

    Overrides ToString()

---
+++ 0x06-csharp-classes/5-enemy/5-enemy.cs
@@ -0,0 +1,54 @@
+﻿using System;
+
+namespace Enemies
+{
+    /// <summary>
+    /// Represents a Zombie.
+    /// </summary>
+    class Zombie 
+    {
+        int health;
+        public string Name { get; set; }
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="Zombie"/> class.
+        /// </summary>
+        public Zombie()
+        {
+            health = 0;
+            Name = "(No name)";
+        }
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="Zombie"/> class.
+        /// </summary>
+        /// <param name="value">The Zombie's health value.</param>
+        public Zombie(int value)
+        {
+            if (value < 0)
+            {
+                throw new System.ArgumentException("Health must be greater than or equal to 0");
+            }
+            health = value;
+            Name = "(No name)";
+        }
+
+        /// <summary>
+        /// Gets the health of the zombie.
+        /// </summary>
+        /// <returns>The health of the zombie.</returns>
+        public int GetHealth()
+        {
+            return health;
+        }
+
+        /// <summary>
+        /// Gets a string of the Zombie's attributes.
+        /// </summary>
+        /// <returns>String of the Zombie's attributes.</returns>
+        public override string ToString()
+        {
+            return String.Format("Zombie Name: {0} / Total Health: {1}", Name, health);
+        }
+    }
+}
