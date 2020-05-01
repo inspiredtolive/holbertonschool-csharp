c7d7f9a16f37c9d84bc1f4ba090c537d3759508f
Marco Chan <inspiredtolive0@gmail.com>
2020-04-30 20:02:25 -0700

    Creates a new Zombie constructor with a param

---
+++ 0x06-csharp-classes/2-enemy/2-enemy.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace Enemies
+{
+    /// <summary>
+    /// Represents a Zombie.
+    /// </summary>
+    class Zombie 
+    {
+        public int health;
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="Zombie"/> class.
+        /// </summary>
+        public Zombie()
+        {
+            health = 0;
+        }
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="Zombie"/> class.
+        /// </summary>
+        /// <param name="value">The Zombie's health value.</param>
+        public Zombie(int value)
+        {
+            if (value < 0)
+            {
+                throw new System.ArgumentException("Health must be greater than or equal to 0");
+            }
+            health = value;
+        }
+    }
+}
