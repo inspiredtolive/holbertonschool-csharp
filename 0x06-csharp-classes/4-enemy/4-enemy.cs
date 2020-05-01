ccbcd1343ceb421aaee88f958af6ba57905f9d58
Marco Chan <inspiredtolive0@gmail.com>
2020-04-30 21:16:48 -0700

    Adds getter and setter for Zombie name

---
+++ 0x06-csharp-classes/4-enemy/4-enemy.cs
@@ -0,0 +1,45 @@
+﻿using System;
+
+namespace Enemies
+{
+    /// <summary>
+    /// Represents a Zombie.
+    /// </summary>
+    class Zombie 
+    {
+        int health;
+        public string Name { get; set; }
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="Zombie"/> class.
+        /// </summary>
+        public Zombie()
+        {
+            health = 0;
+            Name = "(No name)";
+        }
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="Zombie"/> class.
+        /// </summary>
+        /// <param name="value">The Zombie's health value.</param>
+        public Zombie(int value)
+        {
+            if (value < 0)
+            {
+                throw new System.ArgumentException("Health must be greater than or equal to 0");
+            }
+            health = value;
+            Name = "(No name)";
+        }
+
+        /// <summary>
+        /// Gets the health of the zombie.
+        /// </summary>
+        /// <returns>The health of the zombie.</returns>
+        public int GetHealth()
+        {
+            return health;
+        }
+    }
+}
