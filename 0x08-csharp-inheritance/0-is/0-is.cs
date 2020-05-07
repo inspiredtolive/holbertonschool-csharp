f57bbfb0c85f4bc42221549f05b5355689869ccf
Marco Chan <inspiredtolive0@gmail.com>
2020-05-07 16:07:26 -0700

    Checks if an obj is an int

---
+++ 0x08-csharp-inheritance/0-is/0-is.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+
+/// <summary>
+/// Provides obj type checking.
+/// </summary>
+class Obj
+{
+    /// <summary>
+    /// Checks if an obj is an int.
+    /// </summary>
+    /// <param name="obj">The obj to check.</param>
+    /// <returns>True if the object is an int or False.</returns>
+    public static bool IsOfTypeInt(object obj)
+    {
+        return obj is int;
+    }
+}
