ca1b84b34bed4e9713645f82daf8e079a1a0084a
Marco Chan <inspiredtolive0@gmail.com>
2020-05-07 16:31:02 -0700

    Checks if an obj is an instance of an Array

---
+++ 0x08-csharp-inheritance/1-instance/1-instance.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+
+/// <summary>
+/// Provides obj type checking.
+/// </summary>
+class Obj
+{
+    /// <summary>
+    /// Checks if an obj is an instance of an Array.
+    /// </summary>
+    /// <param name="obj">The object to check.</param>
+    /// <returns>True if it is an instance or False.</returns>
+    public static bool IsInstanceOfArray(object obj)
+    {
+        return obj is Array;
+    }
+}
