00c18debbb062c3cd548f53f0f215d376493845a
Marco Chan <inspiredtolive0@gmail.com>
2020-05-07 19:44:56 -0700

    Creates Animal and Dog classes

---
+++ 0x08-csharp-inheritance/4-inherit/4-inherit.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+
+/// <summary>
+/// Represents an animal.
+/// </summary>
+class Animal
+{
+}
+
+/// <summary>
+/// Represents a dog.
+/// </summary>
+class Dog : Animal
+{
+}
