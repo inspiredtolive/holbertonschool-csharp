18ac5411593b4c60b2142bd5840677618a9c689c
Marco Chan <inspiredtolive0@gmail.com>
2020-05-07 17:34:35 -0700

    Checks if a type is a subclass of another

---
+++ 0x08-csharp-inheritance/2-subclass/2-subclass.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+/// <summary>
+/// Provides object type checking.
+/// </summary>
+class Obj
+{
+    /// <summary>
+    /// Checks if a type is a subclass of another type.
+    /// </summary>
+    /// <param name="derivedType">The derived type.</param>
+    /// <param name="baseType">The base type to check against.</param>
+    /// <returns>True if the derived type is a subclass or False.</returns>
+    public static bool IsOnlyASubclass(Type derivedType, Type baseType)
+    {
+        return derivedType.IsSubclassOf(baseType);
+    }
+}
