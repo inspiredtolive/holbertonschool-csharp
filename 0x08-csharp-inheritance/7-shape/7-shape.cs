9281a63c86e922753801e732850b8482c485ace1
Marco Chan <inspiredtolive0@gmail.com>
2020-05-07 22:55:38 -0700

    Adds Area() and ToString() methods

---
+++ 0x08-csharp-inheritance/7-shape/7-shape.cs
@@ -0,0 +1,83 @@
+﻿using System;
+
+/// <summary>
+/// Represents a shape.
+/// </summary>
+class Shape
+{
+    /// <summary>
+    /// Computes the area, not implemented.
+    /// </summary>
+    /// <returns>The area.</returns>
+    public virtual int Area()
+    {
+        throw new NotImplementedException("Area() is not implemented");
+    }
+}
+
+/// <summary>
+/// Represents a Rectangle.
+/// </summary>
+class Rectangle : Shape
+{
+    private int width;
+    private int height;
+
+    /// <summary>
+    /// Gets or sets the width.
+    /// </summary>
+    /// <value>The value to set the width to.</value>
+    public int Width
+    {
+        get
+        {
+            return width;
+        }
+        set
+        {
+            if (value < 0)
+            {
+                throw new ArgumentException("Width must be greater than or equal to 0");
+            }
+            width = value;
+        }
+    }
+
+    /// <summary>
+    /// Gets or sets the height.
+    /// </summary>
+    /// <value>The value to set the height to.</value>
+    public int Height
+    {
+        get
+        {
+            return height;
+        }
+        set
+        {
+            if (value < 0)
+            {
+                throw new ArgumentException("Height must be greater than or equal to 0");
+            }
+            height = value;
+        }
+    }
+
+    /// <summary>
+    /// Computes the area of the Rectangle.
+    /// </summary>
+    /// <returns>The area.</returns>
+    public new int Area()
+    {
+        return Height * Width;
+    }
+
+    /// <summary>
+    /// Provides a new string representation of the Rectangle.
+    /// </summary>
+    /// <returns>The string representation.</returns>
+    public override string ToString()
+    {
+        return String.Format("[Rectangle] {0} / {1}", Width, Height); 
+    }
+}
