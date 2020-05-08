c92f50ca46f7777612d498ef8ad809f5249ecb4b
Marco Chan <inspiredtolive0@gmail.com>
2020-05-07 20:06:19 -0700

    Computes the area of a shape

---
+++ 0x08-csharp-inheritance/5-shape/5-shape.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+/// <summary>
+/// Represents a shape.
+/// </summary>
+class Shape
+{
+    /// <summary>
+    /// Computes the area, not implemented.
+    /// </summary>
+    /// <returns>The area.</returns>
+    public virtual int Area()
+    {
+        throw new NotImplementedException("Area() is not implemented");
+    }
+}
