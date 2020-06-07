b53297f1c876386b3411a086ec72439d0f431f91
Marco Chan <inspiredtolive0@gmail.com>
2020-06-06 17:29:20 -0700

    Safely divides two ints

---
+++ 0x04-csharp-exceptions/1-divide_print/1-divide_print.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+class Int
+{
+    public static void divide(int a, int b)
+    {
+        int quotient = 0;
+
+        try
+        {
+            quotient = a / b;
+        }
+        catch
+        {
+            Console.WriteLine("Cannot divide by zero");
+        }
+        finally
+        {
+            Console.WriteLine("{0} / {1} = {2}", a, b, quotient);
+        }
+    }
+}
