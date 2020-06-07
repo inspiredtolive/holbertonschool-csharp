55f23ffd91bdb63e994c403ead6ace82d996e77a
Marco Chan <inspiredtolive0@gmail.com>
2020-06-06 17:06:43 -0700

    Safe prints a list

---
+++ 0x04-csharp-exceptions/0-safe_list_print/0-safe_list_print.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+
+class List
+{
+    public static int SafePrint(List<int> myList, int n)
+    {
+        int count = 0;
+
+        try
+        {
+            for (int i = 0; i < n; i++, count++)
+            {
+                Console.WriteLine(myList[i]);
+            }
+            return count;
+        }
+        catch (System.Exception)
+        {
+            return count;
+        }
+    }
+}
