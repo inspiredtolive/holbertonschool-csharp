c90c3d8bf2340d49f32add54ee6330385a8b01b7
Marco Chan <inspiredtolive0@gmail.com>
2020-07-27 13:47:14 -0700

    Gets the queue's type

---
+++ 0x0A-csharp-generics/0-queue/queue.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+/// <summary>
+/// Represents a queue.
+/// </summary>
+/// <typeparam name="T">The type of queue.</typeparam>
+public class Queue<T>
+{
+    /// <summary>
+    /// Gets the queue's type.
+    /// </summary>
+    /// <returns>The type of the queue.</returns>
+    public Type CheckType()
+    {
+        return typeof(T);
+    }
+}
