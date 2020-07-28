5ced62e7dffe29e4c5f1694bc4a92c074e93b86c
Marco Chan <inspiredtolive0@gmail.com>
2020-07-27 20:18:44 -0700

    Creates an enqueue method

---
+++ 0x0A-csharp-generics/1-enqueue/queue.cs
@@ -0,0 +1,75 @@
+﻿using System;
+
+/// <summary>
+/// Represents a queue.
+/// </summary>
+/// <typeparam name="T">The type of queue.</typeparam>
+public class Queue<T>
+{
+    private Node head = null;
+    private Node tail = null;
+    private int count = 0;
+    
+    /// <summary>
+    /// Gets the queue's type.
+    /// </summary>
+    /// <returns>The type of the queue.</returns>
+    public Type CheckType()
+    {
+        return typeof(T);
+    }
+
+    /// <summary>
+    /// Gets the number of nodes in the queue.
+    /// </summary>
+    /// <returns>The number of nodes.</returns>
+    public int Count()
+    {
+        return count;
+    }
+
+    /// <summary>
+    /// Creates and adds a node to the end of the queue.
+    /// </summary>
+    /// <param name="value">The value of the new node.</param>
+    public void Enqueue(T value)
+    {
+        Node newNode = new Node(value);
+
+        if (count == 0)
+        {
+            head = newNode;
+            tail = newNode;
+        }
+        else
+        {
+            tail.next = newNode;
+            tail = newNode;
+        }
+        count++;
+    }
+
+    /// <summary>
+    /// Represents a node.
+    /// </summary>
+    public class Node
+    {
+        /// <summary>
+        /// The value of the node.
+        /// </summary>
+        public T value = default(T);
+        /// <summary>
+        /// The next node in the queue.
+        /// </summary>
+        public Node next = null;
+
+        /// <summary>
+        /// The Node constructor to set initial values.
+        /// </summary>
+        /// <param name="nodeValue">The value the node should hold.</param>
+        public Node(T nodeValue)
+        {
+            value = nodeValue;
+        }
+    }
+}
