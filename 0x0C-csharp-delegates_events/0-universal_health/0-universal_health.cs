19a0e7a6ddb4d31fbd24edd5cff3bfb6780db67a
Marco Chan <inspiredtolive0@gmail.com>
2020-07-29 22:46:35 -0700

    Create a Player class w/ a PrintHealth() method

---
+++ 0x0C-csharp-delegates_events/0-universal_health/0-universal_health.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+///<summary>Represents a Player.</summary>
+class Player
+{
+    string name;
+    float maxHp;
+    float hp;
+
+    ///<summary>The Player contructor.</summary>
+    public Player(string name = "Player", float maxHp = 100f)
+    {
+        this.name = name;
+        if (maxHp <= 0)
+        {
+            this.maxHp = 100f;
+            Console.WriteLine("maxHp must be greater than 0. maxHp set to 100f by default.");
+        }
+        else
+        {
+            this.maxHp = maxHp;
+        }
+        this.hp = this.maxHp;
+    }
+
+    ///<summary>Print health of the Player.</summary>
+    public void PrintHealth()
+    {
+        Console.WriteLine(this.name + " has " + this.hp + " / " + this.maxHp + " health");
+    }
+}
