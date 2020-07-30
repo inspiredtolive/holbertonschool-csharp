476b0544a019f9f31c2b10801717317bfa02d83c
Marco Chan <inspiredtolive0@gmail.com>
2020-07-29 23:02:02 -0700

    Creates and uses ValidateHP() to set Player's hp

---
+++ 0x0C-csharp-delegates_events/2-validation/2-validation.cs
@@ -0,0 +1,63 @@
+﻿using System;
+
+///<summary>Represents a Player.</summary>
+class Player
+{
+    string name;
+    float maxHp;
+    float hp;
+
+    ///<summary>The Player contructor.</summary>
+    public Player(string name = "Player", float maxHp = 100f)
+    {
+        this.name = name;
+        if (maxHp <= 0)
+        {
+            this.maxHp = 100f;
+            Console.WriteLine("maxHp must be greater than 0. maxHp set to 100f by default.");
+        }
+        else
+        {
+            this.maxHp = maxHp;
+        }
+        this.hp = this.maxHp;
+    }
+
+    ///<summary>Print health of the Player.</summary>
+    public void PrintHealth()
+    {
+        Console.WriteLine(this.name + " has " + this.hp + " / " + this.maxHp + " health");
+    }
+
+    ///<summary>Damages the Player.</summary>
+    public void TakeDamage(float damage)
+      {
+        if (damage < 0f)
+            damage = 0f;
+        Console.WriteLine($"{this.name} takes {damage} damage!");
+        float newHp = this.hp - damage;
+        this.ValidateHP(newHp);
+    }
+
+    ///<summary>Heals the Player.</summary>
+    public void HealDamage(float heal)
+	{
+		float newHp = this.hp;
+		if (heal < 0f)
+			heal = 0f;
+		Console.WriteLine("{0} heals {1} HP!", this.name, heal);
+		newHp += heal;
+		ValidateHP(newHp);
+	}
+
+    ///<summary>Sets the Player's hp.</summary>
+    public void ValidateHP(float newHp)
+    {
+        if (newHp < 0)
+            this.hp = 0;
+        else if (newHp > this.maxHp)
+            this.hp = this.maxHp;
+        else 
+            this.hp = newHp;
+    }
+}
